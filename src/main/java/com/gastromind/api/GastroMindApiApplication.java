1959314dd3155952c411afbab70b25682d5c9294
GuillermoSH <siciliahernandezguillermo@gmail.com>
2026-02-09 16:08:41 +0000

    :white_check_mark: Creado la estructura inicial; closes #9

---
+++ src/main/java/com/gastromind/api/GastroMindApiApplication.java
@@ -0,0 +1,13 @@
+package com.gastromind.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastroMindApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastroMindApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gastromind/api/GastroMindApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gastromind.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GastroMindApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
