6b9a897a9ca4cecc8c38240ce7467fb77ecba7d5
DavidRiccio <dricciodeabreu1@gmail.com>
2026-02-10 18:22:28 +0000

    fix: Se agregan los repositorios de fridge

---
+++ src/main/java/com/gastromind/api/infrastructure/adapters/out/persistence/postgreSQL/FridgeAdapter.java
@@ -0,0 +1,41 @@
+package com.gastromind.api.infrastructure.adapters.out.persistence.postgreSQL;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.gastromind.api.domain.models.Fridge;
+import com.gastromind.api.domain.repositories.Fridgerepository;
+import com.gastromind.api.infrastructure.adapters.out.persistence.postgreSQL.jpa.FridgeJpaRepository;
+
+public class FridgeAdapter  implements Fridgerepository{
+
+    @Autowired
+    FridgeJpaRepository fridgeJpaRepository;
+
+    @Override
+    public Fridge save(Fridge fridge) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
+
+    @Override
+    public Optional<Fridge> findById(String id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
+
+    @Override
+    public void deleteById(String id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
+    }
+
+    @Override
+    public List<Fridge> findAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+    
+}
